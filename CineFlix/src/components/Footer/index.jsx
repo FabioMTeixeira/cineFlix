8d3f4d3141c128e68b77d54edc27c3e163b39d9d
Fabio <fabiomt.98@hotmail.com>
2023-10-16 13:27:06 -0300

    chore: adicionado layout para o footer

--- CineFlix/src/components/Footer/index.jsx
+++ CineFlix/src/components/Footer/index.jsx
@@ -1,8 +1,18 @@
 import './style.css';
+import Logo from '../../assets/logo.svg';
 
 function Footer() {
     return (
-        <h1>Footer</h1>
+        <footer>
+            <div className='footer-left'>
+                <img src={Logo} alt="logo" />
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptate in rerum sequi, soluta neque temporibus, blanditiis tempora quibusdam quis dolore autem necessitatibus quidem tempore recusandae qui commodi hic dicta?</p>
+            </div>
+
+            <div className='footer-right'>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptate in rerum sequi, soluta neque temporibus, blanditiis tempora quibusdam quis dolore autem necessitatibus quidem tempore recusandae qui commodi hic dicta?</p>
+            </div>
+        </footer>
     );
 }
 
